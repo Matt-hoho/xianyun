659558580b3761c9e33b8b4ceac317de16adb2c5
Matt-hoho <Matt_hoho@163.com>
2019-10-15 15:59:19 +0800

    航班列表与分页

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -7,12 +7,31 @@
         <div></div>
 
         <!-- 航班头部布局 -->
-        <div></div>
+        <FlightsListHead />
 
         <!-- 航班信息 -->
-        <div></div>
-      </div>
+        <div v-loading="loading">
+          <!-- 航班列表 -->
+          <flightsItem v-for="(item,index) in dataList" :key="index" :data="item" />
 
+          <!-- 分页 -->
+          <el-row type="flex" justify="center" style="margin-top:10px;" v-if="loading">
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="pageIndex"
+              :page-sizes="[5, 10, 15, 20]"
+              :page-size="pageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="flightsData.total"
+            ></el-pagination>
+          </el-row>
+        </div>
+        <div
+          v-if="flightsData.flights.length === 0 && !loading"
+          style="padding: 50px; text-align:center"
+        >该航班暂无数据</div>
+      </div>
       <!-- 侧边栏 -->
       <div class="aside">
         <!-- 侧边栏组件 -->
@@ -22,11 +41,52 @@
 </template>
 
 <script>
-import moment from "moment";
-
+import FlightsListHead from "@/components/air/flightsListHead.vue";
+import FlightsItem from "@/components/air/flightsItem.vue";
 export default {
   data() {
-    return {};
+    return {
+      flightsData: {
+        flights: []
+      },
+      pageIndex: 1,
+      pageSize: 5,
+      loading: true
+    };
+  },
+  components: {
+    FlightsListHead,
+    FlightsItem
+  },
+  computed: {
+    dataList() {
+      const arr = this.flightsData.flights.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageIndex * this.pageSize
+      );
+      return arr;
+    }
+  },
+  methods: {
+    getData() {
+      this.$axios({
+        url: "airs",
+        params: this.$route.query
+      }).then(res => {
+        this.flightsData = res.data;
+        this.loading = false;
+      });
+    },
+
+    handleSizeChange(val) {
+      this.pageSize = val;
+    },
+    handleCurrentChange(val) {
+      this.pageIndex = val;
+    }
+  },
+  mounted() {
+    this.getData();
   }
 };
 </script>
